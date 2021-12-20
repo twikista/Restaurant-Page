1772fa2a5e209781147b6b055685062d0deabde7
viro <twikista@gmail.com>
2021-12-21 00:12:50 +0100

    configured webpack to load images

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import "./style.css";
+import avatar from "./AvatarMaker2.png";
 console.log("Hey!, i can see you");
 const h1 = document.createElement("h1");
 h1.textContent = "I am an H1 element";
+const myImage = new Image();
+myImage.src = avatar;
 document.body.appendChild(h1);
+document.body.appendChild(myImage);
