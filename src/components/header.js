0d753f7cd05a3544e7e6b525dc881e24b8d93ab9
viro <twikista@gmail.com>
2021-12-22 01:06:05 +0100

    added  the header and home modules

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,41 @@
 function createHeader() {
   const header = document.createElement("header");
+  header.append(createNavbar());
+  return header;
 }
 
 function createNavbar() {
-  const navbar = document.createElement("nav");
+  const navBar = document.createElement("nav");
+  navBar.classList.add("nav-bar");
+  const logo = document.createElement("h2");
+  logo.textContent = "Havilah.";
+  //menu buttons
+  const navMenu = document.createElement("ul");
+  navMenu.classList.add("nav-menu");
+  const fragment = new DocumentFragment();
+  const menuItems = ["home", "menu", "contact"];
+  menuItems.forEach((item) => {
+    const li = document.createElement("li");
+    li.classList.add("list-item");
+    const button = document.createElement("button");
+    button.classList.add("nav-btn");
+    button.textContent = item;
+    li.append(button);
+    fragment.append(li);
+  });
+  navMenu.append(fragment);
+  //create menu toggle button
+  const toggleBtn = document.createElement("button");
+  toggleBtn.classList.add("toggle-btn");
+  toggleBtn.textContent = "menu";
+  //append components to nav bar
+  navBar.append(logo, navMenu, toggleBtn);
+  return navBar;
 }
+
+function launchHeader() {
+  const main = document.querySelector("main");
+  main.append(createHeader());
+}
+
+export default launchHeader;
---
+++ src/components/tabs/home.js
@@ -0,0 +1,32 @@
+function homeSection() {
+  //create element
+  const sectionWrapper = document.createElement("section");
+  const sectionContent = document.createElement("div");
+  sectionContent.classList.add("section-content");
+  //create main section header
+  const h2 = document.createElement("h2");
+  h2.textContent = "tasty meals for every moment";
+  h2.classList.add("section-heading");
+  //create main section text
+  const paragraph = document.createElement("p");
+  paragraph.textContent =
+    "For over two decades, Havilah has served delicious healthy meals to over 20,000 uniqiue customers from across the globe. Our rich menu and superior cusotmer service has receives several industry awards as the best place to eat. ";
+  paragraph.classList.add("section-text");
+  //create link to menu section
+  const link = document.createElement("a");
+  link.href = "#menu-section";
+  link.textContent = "explore our menu";
+  //append elements
+  sectionContent.append(h2, paragraph, link);
+  const fragment = new DocumentFragment();
+  fragment.append(sectionContent);
+  sectionWrapper.append(fragment);
+  return sectionWrapper;
+}
+
+function launchHome() {
+  const main = document.querySelector("main");
+  main.append(homeSection());
+}
+
+export default launchHome;
